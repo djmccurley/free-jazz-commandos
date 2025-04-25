8885cc7760a486243171305a32f4a60ac2afb056
Dan McCurley <dmccurley@dans-mbp.misc.iastate.edu>
2025-04-25 10:58:28 -0500

    player setup 1 and 2 working

--- sketch.js
+++ sketch.js
@@ -1,37 +1,54 @@
-let players, player1;
-let p1Sprite;
+let players, player1, player2;
+let p1Sprite, p2Sprite;
 
 function preload() {
 	new Canvas(480, 270);
-	displayMode('maxed', 'pixelated', 4);
+	displayMode('maxed', 'pixelated');
 	p1Sprite = loadImage("sprites/player1b.png");
+	p2Sprite = loadImage("sprites/player2.png");
 
 }
 
 function setup() {
 
 	players = new Group();
-	players.spriteSheet = p1Sprite;
-  	players.anis.offset.x = 0;
-  	players.anis.frameDelay = 8;
   	players.friction = 0;
  	players.layer = 0;
 	players.width = 24;
 	players.height = 32;
-	players.addAnis({
-		idle: { row: 0, frames: 4},
-		walk: { row: 1, frames: 4},
-		strafe: { row: 3, frames: 4},
-		jump: { row: 9, frames: 4}
-	});
+	players.rotationLock = true;
+	players.addSprite = function(sheet) {
+		this.spriteSheet = sheet;
+		this.addAnis({
+			idle: { row: 0, frames: 4},
+			strafe: { row: 1, frames: 4},
+			walk: { row: 2, frames: 4},
+			punch1: { row: 3, frames: 1},
+			punch2: { row: 4, frames: 1},
+			punch3: { row: 5, frames: 1},
+			headbutt: { row: 6, frames: 2},
+			kick1: { row: 7, frames: 1},
+			kick2: { row: 8, frames: 1},
+			jump: { row: 9, frames: 4},
+			climb: { row: 10, frames: 4}
+		});
+		this.changeAni('idle');
+		this.anis.offset.x = 0;
+		this.anis.frameDelay = 8;
+	}
+
+
 
 	player1 = new players.Sprite();
-	player1.changeAni('jump');
+	player1.addSprite(p1Sprite);
+	
+
+	player2 = new players.Sprite();
+	player2.addSprite(p2Sprite);
 
 	allSprites.pixelPerfect = true;
 }
 
 function draw() {
 	background('skyblue');
-	rect(10,20,16,16);
 }
